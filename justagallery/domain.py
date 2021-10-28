3fabf1b26e956717d4092910fa7c63e8cfcf96b4
Bastiaan Welmers <bastiaan@welmers.net>
2021-10-29 01:17:57 +0200

    Implement category <=> url lookup functions

---
+++ justagallery/domain.py
@@ -0,0 +1,25 @@
+from typing import Dict, Optional
+
+from . import models
+
+_URLS_TO_CATEGORY: Dict[str, models.Category] = {}
+
+
+def get_category_by_url(url: str) -> Optional[models.Category]:
+	url_parts = url.split('/')
+	try:
+		category = None
+		for url_part in url_parts:
+			category = models.Category.objects.filter(parent=category, slug=url_part).first()
+		return category
+	except models.Category.DoesNotExist:
+		return None
+
+
+def get_url_by_category(category: models.Category) -> str:
+	url_parts = []
+	while(category):
+		url_parts.append(category.slug)
+		category = category.parent
+	url_parts.reverse()
+	return '/' + '/'.join(url_parts) + '/'
