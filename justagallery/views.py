a88a5ef61a23ce335c500333f3667af554806222
Bastiaan Welmers <bastiaan@welmers.net>
2021-10-29 00:19:20 +0200

    First index page

---
+++ justagallery/views.py
@@ -0,0 +1,8 @@
+from django.http import HttpRequest, HttpResponse
+from django.shortcuts import render
+
+from . import models
+
+def index(request: HttpRequest) -> HttpResponse:
+	categories = models.Category.objects.filter(parent=None).order_by('-created_at').all()
+	return render(request, 'index.html.j2', dict(categories=categories), using='jinja2')
