e88a8f98e4a8263b6e0b3a5118e618e8c455758e
Bastiaan Welmers <bastiaan@welmers.net>
2021-10-31 15:02:01 +0100

    Add filters and search fields

--- justagallery/admin.py
+++ justagallery/admin.py
@@ -14,12 +14,16 @@ class CategoryAdmin(admin.ModelAdmin):
 	fields = ['parent', 'title', 'description', 'slug', 'default_thumbnail_format', 'display_formats']
 	list_display = ('title', 'parent', 'slug', 'created_at', 'updated_at')
 	ordering = ('-parent', '-created_at', )
+	list_filter = ('parent',)
+	search_fields = ('title',)
 
 class ImageAdmin(admin.ModelAdmin):
 	model = models.Image
 	fields = ['category', 'title', 'description', 'file', 'display_formats']
 	list_display = ('title', 'category', 'created_at', 'updated_at')
 	ordering = ('-category', '-created_at', )
+	list_filter = ('category',)
+	search_fields = ('title',)
 
 admin.site.register(models.ThumbnailFormat, ThumbnailFormatAdmin)
 admin.site.register(models.Category, CategoryAdmin)
